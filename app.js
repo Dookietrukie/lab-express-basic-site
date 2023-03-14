3914d87330a7056180d3093d31a3ee5f2d12d54d
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-14 17:49:27 +0100

    Implemented works works works works works page

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.get("/about", (req, res, next) => {
   res.sendFile(__dirname + "/views/about.html");
 });
 
+// Works
+app.get("/works", (req, res, next) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+//Gallery
+app.get("/gallery", (req, res, next) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
 // Set up listener
 app.listen(3000, () => {
   console.log("Listening... PORT 3000.");
